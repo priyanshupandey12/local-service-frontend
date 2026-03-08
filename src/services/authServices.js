9429f3c295dc38e21f758d0b7a4891bf081d2bf5
priyanshupandey12 <priyanshupandat32@gmail.com>
2026-03-08 15:30:52 +0530

    fixing the bug

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,23 +1,20 @@
-import { API } from "../utils/api";
-import useAuthStore from "../store/authStore";
 
-export const login = async (data) => {
-  const response = await API.post("/auth/login", data);
-  const { token, userId, role } = response.data;
-  useAuthStore.getState().setAuth(userId, role, token);
-  return response.data;
-};
+import { API } from "../utils/api";
+
 
 export const register = async (data) => {
   const response = await API.post("/auth/register", data);
-  const { token, userId, role } = response.data;
-  useAuthStore.getState().setAuth(userId, role, token);
+  return response.data;
+};
+
+export const login = async (data) => {
+  const response = await API.post("/auth/login", data);
   return response.data;
 };
 
 export const logout = async () => {
-  await API.post("/auth/logout");
-  useAuthStore.getState().clearAuth();
+  const response = await API.post("/auth/logout");
+  return response.data;
 };
 
 export const getMe = async () => {
